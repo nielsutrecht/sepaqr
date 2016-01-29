7b4a653b130d5badcb8d9c8b1909e9d98b96841d
Niels Dommerholt <niels.dommerholt@jdriven.com>
2016-01-29 17:22:11 +0100

    Halfway working version

--- app/app.js
+++ app/app.js
@@ -1,13 +1,50 @@
 var appModule = angular.module('sepaQr', ['ngRoute']);
 
-appModule.config(['$httpProvider', function($httpProvider) {
-    $httpProvider.interceptors.push('httpInterceptor');
+appModule.controller('MainCtrl', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
+    $scope.banks = {};
+
+
+    $http.get('app/banks.json').then(function(result) {
+        $scope.banks = result.data;
+    });
+
+    $scope.selectBank = function() {
+        $scope.bankName = $scope.banks[$scope.bank].bank_name;
+        $scope.bankBic = $scope.banks[$scope.bank].BIC;
+        $scope.updateQr();
+    }
+
+
+    $scope.updateQr = function() {
+        var iban = 'NL' + $scope.ibanControl + $scope.bank + $scope.ibanAccount;
+        var payload = [
+        	'BCD',
+          '002',
+          '1',
+          'SCT',
+          $scope.bankBic,
+          $scope.name,
+          iban,
+          'EUR' + $scope.amount,
+          'SEPA',
+          '',
+          '',
+          ''].join('\n');
+        if(!$scope.qrcode) {
+            $scope.qrcode = new QRCode(document.getElementById("qrcode"), {
+                                            text: payload,
+                                            width: 256,
+                                            height: 256,
+                                            colorDark : "#000000",
+                                            colorLight : "#ffffff",
+                                            correctLevel : QRCode.CorrectLevel.H
+                                        });
+        }
+        else {
+            $scope.qrcode.makeCode(payload);
+        }
+    }
 }]);
-
-appModule.controller('MainCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
-    $scope.bank = $routeParams.bank
-
-]);
 
 appModule.config(['$routeProvider',
     function($routeProvider, config) {
